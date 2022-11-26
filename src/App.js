d4726648d2ad02553473f53cf4b1ba0057b7444f
“Tushar” <abc@example.com>
2022-11-26 15:38:39 +0530

    fixed matched route

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ const App = () => (
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
-        <Route path="/video:id" element={<VideoDetails />} />
-        <Route path="/channel:id" element={<ChannelDetails />} />
-        <Route path="/search:searchTerm" element={<SearchFeed />} />
+        <Route path="/video/:id" element={<VideoDetails />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
