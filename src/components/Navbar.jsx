771d02a8030251aa7e163c0756a6b3d92816377c
abc <abc@example.com>
2022-11-26 10:28:30 +0530

    added search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
-
+import SearchBar from './SearchBar';
 const Navbar = () => (
   <div>
     <Stack
@@ -15,11 +15,13 @@ const Navbar = () => (
         top: 0,
         justifyContent: 'Space-between',
       }}
-    />
+    >
 
     <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
+    </Stack>
   </div>
 );
 
