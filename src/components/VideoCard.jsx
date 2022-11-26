20cd4e741c7b3d9f8b4a39ed119b53662cce6ef0
“Tushar” <abc@example.com>
2022-11-26 12:44:19 +0530

    added video card layout

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelTitle,
+  demoChannelUrl,
+} from '../utils/constants';
 
-function VideoCard() {
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
   return (
-    <div>VideoCard</div>
-  )
-}
+    <>
+      <Card>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <CardMedia
+            image={snippet?.thumbanils?.high.url}
+            alt={snippet.tittle}
+            sx={{ width: 358, height: 180 }}
+          ></CardMedia>
+          <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+            <Link
+              to={
+                snippet?.channelId
+                  ? `/channel/${snippet?.channelId}`
+                  : demoChannelUrl
+              }
+            >
+              <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+                {snippet.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+              </Typography>
+            </Link>
+          </CardContent>
+        </Link>
+      </Card>
+    </>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
