e2cac796f1b6dc009dea7ad6bd982c6fc01e5686
“Tushar” <abc@example.com>
2022-11-26 17:23:06 +0530

    added video player

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import ReactPlayer from 'react-player';
+import { useParams } from 'react-router-dom';
+import { fetchAPI } from '../utils/fetchAPI';
 
-function VideoDetails() {
+const VideoDetails = () => {
+  const { id } = useParams();
+  const [videosDetails, setVideosDetails] = useState(null);
+
+  useEffect(() => {
+    fetchAPI(`videos?part=snippet&statistics&id=${id}`).then((data) =>
+      setVideosDetails(data?.items[0])
+    );
+  }, [id]);
+
   return (
-    <div>VideoDetail</div>
-  )
-}
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box sx={{ width: '100%', position: 'sticky', top: '80px' }}>
+          <ReactPlayer
+            url={`https://wwww.youtube.com/watch?v=${id}`}
+            className="react-player"
+            controls
+          />
+          <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            {videosDetails?.snippet?.title}
+          </Typography>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
 
-export default VideoDetails
+export default VideoDetails;
